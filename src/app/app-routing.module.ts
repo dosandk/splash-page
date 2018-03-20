ae3eda8d68bb94933ec8af5b9ea2fabbd6eefcdc
Vladimir <dosandkv@gmail.com>
2018-03-21 00:46:05 +0300

    Added basic structure for routing;

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SplashPageComponent } from './splash-page/splash-page.component';
+import { AppsListComponent } from './apps-list/apps-list.component';
+
+//  TODO: maybe we will have routes for all existed apps but will show only those to which the user has access
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'splash-page',
+    component: SplashPageComponent,
+    children: [
+      {
+        path: 'apps-list',
+        component: AppsListComponent,
+        // TODO: implement lazy-loading
+        // children: [
+        //   { path: 'app1', component: SomeComponent },
+        //   { path: 'app1', component: SomeComponent },
+        //   { path: 'app1', component: SomeComponent }
+        // ]
+      },
+    ]
+  }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes)],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SplashPageComponent } from './splash-page/splash-page.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppsListComponent } from './apps-list/apps-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SplashPageComponent,
+    AppsListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
